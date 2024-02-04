06b1cf2c4f103d97c7373f327773c0191382bbbf
immanuelcherui1 <mmanuelcherui1@gmail.com>
2024-02-04 22:35:27 +0300

    5th Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import TransactionsTable from "./TransactionsTable"
+
 function App() {
+
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/transactions')
+      .then(res => {
+        return res.json();
+        })
+
+      .then(data => {
+          setTransactions(data);
+        }
+      )
+  }, []);
 
   return (
     <>
     <h1>BANK OF FLATIRON</h1>
+    <TransactionsTable transactions={transactions}/>
     </>
   )
 }
